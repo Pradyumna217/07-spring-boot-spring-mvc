f39c44e0ace2c63429f105ace38cb4bbb1e71a6f
Pradyumna217 <118720435+Pradyumna217@users.noreply.github.com>
2024-12-16 15:40:28 +0530

    Add files via upload

---
+++ 07-spring-boot-spring-mvc/01-thymleafdemo-helloworld/src/main/java/com/learning/springboot/thymleafdemo/model/Student.java
@@ -0,0 +1,60 @@
+package com.learning.springboot.thymleafdemo.model;
+
+import java.util.List;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    private String country;
+    private List<String> favoriteSystems;
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
+    }
+
+
+
+    private String favouriteLanguage;
+
+    public String getFavouriteLanguage() {
+        return favouriteLanguage;
+    }
+
+    public void setFavouriteLanguage(String favouriteLanguage) {
+        this.favouriteLanguage = favouriteLanguage;
+    }
+
+    public Student(){
+
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
